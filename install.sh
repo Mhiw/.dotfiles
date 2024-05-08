ecbed0849b6ef5665c27d0663d0eb73abc445257
Wilhelm Carlsson <103310488+Mhiw@users.noreply.github.com>
2024-05-08 18:36:26 +0200

    Errors

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Add or remove directories (full path) in file "directories.txt" to add to the repo
 directories=($(cat directories.txt | awk '!/#/ {print $0}'))
 
-if [ -d "/home/$USER/.config"]; then
+if [ -d "/home/$USER/.config" ]; then
 	install
 else
 	mkdir /home/$USER/.config
