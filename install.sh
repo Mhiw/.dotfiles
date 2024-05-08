e14c968c66d915bb12f4cee90ed895ee633e8786
Wilhelm Carlsson <103310488+Mhiw@users.noreply.github.com>
2024-05-08 18:35:04 +0200

    Fixed install script

--- install.sh
+++ install.sh
@@ -3,9 +3,18 @@
 # Add or remove directories (full path) in file "directories.txt" to add to the repo
 directories=($(cat directories.txt | awk '!/#/ {print $0}'))
 
-for directory in "${directories[@]}"
-do
-	directory_name=$(basename "$directory")
-	mkdir directory
-	cp directory_name directory
-done
+if [ -d "/home/$USER/.config"]; then
+	install
+else
+	mkdir /home/$USER/.config
+	install
+fi
+
+install() {
+	for directory in "${directories[@]}"
+	do
+		directory_name=$(basename "$directory")
+		mkdir $directory
+		cp $directory_name $directory
+	done
+}
