d718211e07430ee2d3adbc6d2ad4b6290fd10fa2
Wilhelm Carlsson <103310488+Mhiw@users.noreply.github.com>
2024-05-08 18:39:53 +0200

    Wrong params to cp

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@
 # Add or remove directories (full path) in file "directories.txt" to add to the repo
 directories=($(cat directories.txt | awk '!/#/ {print $0}'))
 
+install_config() {
+	for directory in "${directories[@]}"
+	do
+		directory_name=$(basename "$directory")
+		mkdir $directory
+		cp -r $directory_name $directory
+	done
+}
+
 if [ -d "/home/$USER/.config" ]; then
 	install_config
 else
 	mkdir /home/$USER/.config
 	install_config
 fi
-
-install_config() {
-	for directory in "${directories[@]}"
-	do
-		directory_name=$(basename "$directory")
-		mkdir $directory
-		cp $directory_name $directory
-	done
-}
